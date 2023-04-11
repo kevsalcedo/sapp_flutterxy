68e3881bbf4183ab8ec5cde8d3889744a04b97a1
KevSalcedo <kevsalcedom@gmail.com>
2023-04-11 13:03:04 -0500

    Create bottom_bar.dart and include Home and GudPlaces as labels.

---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,60 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    const Text("Home"),
+    const Text("GudPlaces"),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SApp'),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: _widgetOptions[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        elevation: 10,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(FluentIcons.home_24_regular),
+            activeIcon: Icon(FluentIcons.home_24_filled),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FluentIcons.album_24_regular),
+            activeIcon: Icon(FluentIcons.album_24_filled),
+            label: 'GudPlaces',
+          ),
+        ],
+      ),
+    );
+  }
+}
