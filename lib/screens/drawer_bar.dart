70540db89477e78cd2485bdeff2794d66c83380c
KevSalcedo <kevsalcedom@gmail.com>
2023-04-11 13:03:21 -0500

    Update drawer_bar.dart. Need to be fixed.

--- lib/screens/drawer_bar.dart
+++ lib/screens/drawer_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sapp_flutterxy/screens/home_screen.dart';
+import 'package:sapp_flutterxy/screens/profile_screen.dart';
 
 class DrawerBar extends StatefulWidget {
   const DrawerBar({Key? key}) : super(key: key);
@@ -9,14 +10,26 @@ class DrawerBar extends StatefulWidget {
   State<DrawerBar> createState() => _DrawerBarState();
 }
 
+class Destination {
+  final String title;
+  final IconData icon;
+  final String route;
+
+  Destination(this.title, this.icon, this.route);
+}
+
+List<Destination> allDestinations = <Destination>[
+  Destination('Home', Icons.home, '/home'),
+  Destination('Profile', Icons.account_circle, '/profile'),
+  Destination('Settings', Icons.settings, '/settings'),
+  // Agrega más destinos aquí
+];
+
 class _DrawerBarState extends State<DrawerBar> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 1;
   static final List<Widget> _widgetOptions = <Widget>[
     const HomeScreen(),
-    const Text(
-      'Profile',
-      style: TextStyle(fontSize: 30),
-    ),
+    const ProfileScreen(),
     const Text(
       'Settings',
       style: TextStyle(fontSize: 30),
@@ -56,24 +69,21 @@ class _DrawerBarState extends State<DrawerBar> {
                 ),
               ),
             ),
-            ListTile(
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const HomeScreen()));
-              },
-              leading: const Icon(Icons.home),
-              title: const Text('Home'),
-            ),
-            const ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Profile'),
-            ),
-            const ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: allDestinations.map((destination) {
+                return ListTile(
+                  leading: Icon(destination.icon),
+                  title: Text(destination.title),
+                  selected: ModalRoute.of(context)?.settings.name ==
+                      destination.route,
+                  onTap: () {
+                    _onItemTapped(_selectedIndex);
+                    Navigator.pop(context);
+                    //Navigator.pushNamed(context, destination.route);
+                  },
+                );
+              }).toList(),
             ),
           ],
         ),
