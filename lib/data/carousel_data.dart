6ef2dc1e8addfbe4bb797db419b76028495d9553
KevSalcedo <kevsalcedom@gmail.com>
2023-04-11 21:41:06 -0500

    Create carousel_data and include data.

---
+++ lib/data/carousel_data.dart
@@ -0,0 +1,19 @@
+import 'package:sapp_flutterxy/models/model_carousel.dart';
+
+const carouselImages = <Carousel>[
+  Carousel(
+    id: 1,
+    name: "Lavanda",
+    image: "assets/images/lavanda_flower.jpg",
+  ),
+  Carousel(
+    id: 2,
+    name: "Hierba buena",
+    image: "assets/images/hierbabuena_leaves.jpg",
+  ),
+  Carousel(
+    id: 3,
+    name: "Lobelia",
+    image: "assets/images/lobelia_flower.jpg",
+  ),
+];
