101a7165230ea119d600e1028ae34ac60c5f32f6
KevSalcedo <kevsalcedom@gmail.com>
2023-04-11 20:49:05 -0500

    Create Gaps and CarouselSlider. Need to be updated.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,9 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import '../utils/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -7,21 +11,29 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFeeedf2),
+      backgroundColor: Styles.bgColor,
       body: ListView(
         children: [
           Container(
             padding: const EdgeInsets.all(20),
             child: Column(
               children: [
+                const Gap(40),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text("Good Morning!"),
-                        Text("Ask for a Sati!"),
+                      children: [
+                        Text(
+                          "Hi dude!",
+                          style: Styles.headLineStyle3,
+                        ),
+                        const Gap(5),
+                        Text(
+                          "Let's order!",
+                          style: Styles.headLineStyle1,
+                        ),
                       ],
                     ),
                     Container(
@@ -36,6 +48,55 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ),
                   ],
+                ),
+                const Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color(0xfff4f6fd)),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child: Row(
+                    children: [
+                      const Icon(FluentIcons.search_24_regular,
+                          color: Color(0xffBDBDBD)),
+                      Text("What are you looking for?",
+                          style: Styles.headLineStyle4),
+                    ],
+                  ),
+                ),
+                const Gap(25),
+                Container(
+                  height: 200,
+                  width: double.infinity,
+                  child: CarouselSlider(
+                    options: CarouselOptions(
+                      height: 400.0,
+                      initialPage: 0,
+                      enableInfiniteScroll: false,
+                      autoPlay: true,
+                      autoPlayInterval: const Duration(seconds: 3),
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      enlargeCenterPage: true,
+                      enlargeFactor: 0.3,
+                    ),
+                    items: [1, 2, 3].map((i) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return Container(
+                              width: MediaQuery.of(context).size.width,
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 5.0),
+                              decoration:
+                                  const BoxDecoration(color: Colors.amber),
+                              child: Text(
+                                'text $i',
+                                style: const TextStyle(fontSize: 16.0),
+                              ));
+                        },
+                      );
+                    }).toList(),
+                  ),
                 ),
               ],
             ),
