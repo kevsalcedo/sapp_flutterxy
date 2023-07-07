71cfe7147864a41684046d73855181c02d3e0364
KevSalcedo <kevsalcedom@gmail.com>
2023-07-07 13:12:57 -0500

    Add SliverToBoxAdapter to show the body of the store_screen.dart

--- lib/presentation/pages/store_screen.dart
+++ lib/presentation/pages/store_screen.dart
@@ -25,14 +25,121 @@ class StoreScreen extends StatelessWidget {
               ),
             ),
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  color: index.isOdd ? Colors.white : Colors.black12,
-                  height: 100.0,
-                  child: Center(
-                    child: Text('$index', textScaleFactor: 5),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  height: 70,
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image(
+                            image: AssetImage(
+                                "assets/images/medical_plants_store.png"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Card(
+                          elevation: 0,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              // color: Styles.bgColor,
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 3),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                    width: 85,
+                                    child: Card(
+                                      color: Styles.bgColor,
+                                      child: Padding(
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 8, vertical: 2),
+                                        child: Column(
+                                          children: [
+                                            Icon(
+                                              Icons.star,
+                                              color: Colors.grey,
+                                            ),
+                                            Text(
+                                              '4.5 (420)',
+                                              style: TextStyle(
+                                                color: Colors.grey[800],
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 85,
+                                    child: Card(
+                                      color: Styles.bgColor,
+                                      child: Padding(
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 8, vertical: 2),
+                                        child: Column(
+                                          children: [
+                                            Icon(
+                                              Icons.monetization_on_outlined,
+                                              color: Colors.grey,
+                                            ),
+                                            Text(
+                                              '\$ 5.000',
+                                              style: TextStyle(
+                                                color: Colors.grey[800],
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 85,
+                                    child: Card(
+                                      color: Styles.bgColor,
+                                      child: Padding(
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 8, vertical: 2),
+                                        child: Column(
+                                          children: [
+                                            Icon(
+                                              Icons.watch_later_outlined,
+                                              color: Colors.grey,
+                                            ),
+                                            Text(
+                                              '5 min',
+                                              style: TextStyle(
+                                                color: Colors.grey[800],
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 );
               },
